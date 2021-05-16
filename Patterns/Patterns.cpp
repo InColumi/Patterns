cdd95e7062d73e3ffb4b3d407b46d688d7671759
max Icolumi <max77648@gmail.com>
2021-05-17 00:07:01 +0300

    Добавил обработку исключений

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -23,15 +23,49 @@ int main()
 	ClientCode(*creator2);
 	singleton->ShowInfoOnMonitor("invoke method ClientCode");
 
-	delete creator1;
-	singleton->ShowInfoOnMonitor("delete creator1");
-	delete creator2;
-	singleton->ShowInfoOnMonitor("delete creator2");
+	try
+	{
+		delete creator1;
+		singleton->ShowInfoOnMonitor("delete creator1");
+	}
+	catch(...)
+	{
+		singleton->ShowInfoOnMonitor("Error with delete creator1");
+	}
+
+	try
+	{
+		delete creator2;
+		singleton->ShowInfoOnMonitor("delete creator2");
+	}
+	catch(...)
+	{
+		singleton->ShowInfoOnMonitor("Error with delete creator2");
+	}
+	
 	std::cout << "-----------------------------End Abstract Factory-----------------------------\n";
 	std::cout << "-----------------------------Prototype-----------------------------\n";
 
 	PrototypeFactory* prototype_factory = new PrototypeFactory();
-	Client(*prototype_factory);
+	try
+	{
+		Client(*prototype_factory);
+	}
+	catch(...)
+	{
+		singleton->ShowInfoOnMonitor("Error with Client(*prototype_factory)");
+	}
+	
+	try
+	{
+		delete prototype_factory;
+		singleton->ShowInfoOnMonitor("delete prototype_factory");
+	}
+	catch(...)
+	{
+		singleton->ShowInfoOnMonitor("Error with delete prototype_factory");
+	}
+
 	delete prototype_factory;
 	std::cout << "-----------------------------End Prototype-----------------------------\n";
 
